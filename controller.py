0e618a2a7f664000d5ff65bcf2d25fea1c7c0131
Cameron Seebach <cameron.seebach@gmail.com>
2012-04-06 15:50:13 -0700

    Tool interface created: basic drawing commands separated into Pencil tool

--- controller.py
+++ controller.py
@@ -1,7 +1,103 @@
-import pyglet
-
 __author__ = 'cseebach'
 
+class Tool(object):
+    """
+    A base class for each kind of tool available in Pixel Slammer.
+    """
+
+    class MoreInputNeeded(Exception):
+        pass
+
+    def __init__(self, color):
+        self.color = color
+
+    def accept_click(self, x, y):
+        """
+        Send a click to this tool. This method returns True if more input is
+        needed.
+        """
+
+    def accept_drag(self, start_x, start_y, end_x, end_y):
+        """
+        Send a drag to this tool. This method returns True if more input is
+        needed.
+        """
+
+    def do(self, model):
+        """
+        Run the action associated with this tool on the specified model. Throws
+        MoreInputNeeded if more input is needed.
+        """
+
+def plot(model, x, y, color):
+    canvas_w, canvas_h = model.canvas.get_size()
+    tile_w, tile_h = model.canvas.get_tile_size()
+
+    max_x = canvas_w * tile_w
+    max_y = canvas_h * tile_h
+    if x < 0 or y < 0 or x >= max_x or y >= max_y:
+        return
+
+    tile_x, tile_y = x // tile_w, y // tile_h
+    tile_pix_x, tile_pix_y = x % tile_w, y % tile_h
+
+    tile = model.canvas.get_tile(tile_x, tile_y)
+    tile.set_pixel(tile_pix_x, tile_pix_y, color)
+
+def draw_line(model, start_x, start_y, end_x, end_y, color):
+    steep = abs(end_y - start_y) > abs(end_x - start_x)
+    if steep:
+        start_x, start_y = start_y, start_x
+        end_x, end_y = end_y, end_x
+    if start_x > end_x:
+        start_x, end_x = end_x, start_x
+        start_y, end_y = end_y, start_y
+    delta_x = end_x -start_x
+    delta_y = abs(end_y - start_y)
+    error = delta_x / 2
+    y = start_y
+    if start_y < end_y:
+        y_step = 1
+    else:
+        y_step = -1
+    for x in xrange(start_x, end_x+1):
+        if steep:
+            plot(model, y, x, color)
+        else:
+            plot(model, x, y, color)
+        error -= delta_y
+        if error < 0:
+            y += y_step
+            error += delta_x
+
+class Pencil(Tool):
+    """
+    A class for drawing simple lines and points on the canvas.
+    """
+
+    def __init__(self, color):
+        super(Pencil, self).__init__(color)
+        self.style = None
+
+    def accept_click(self, x, y):
+        self.style = "point"
+        self.x = x
+        self.y = y
+
+    def accept_drag(self, start_x, start_y, end_x, end_y):
+        self.style = "line"
+        self.start_x, self.start_y = start_x, start_y
+        self.end_x, self.end_y = end_x, end_y
+
+    def do(self, model):
+        if not self.style:
+            raise (Tool.MoreInputNeeded,
+                   "Pencil needs a click or a drag before do()")
+        elif self.style == "point":
+            plot(model, self.x, self.y, self.color)
+        elif self.style == "line":
+            draw_line(model, self.start_x, self.start_y, self.end_x,
+                      self.end_y, self.color)
 
 class SlammerCtrl(object):
     """
@@ -17,6 +113,10 @@ class SlammerCtrl(object):
         self.view.push_handlers(self)
         self.view.canvas.set_canvas(model.get_canvas())
 
+        self.current_tool = Pencil
+        self.current_tool_instance = None
+        self.current_color = (0,0,0,255)
+
     def get_canvas_pixel(self, x_ratio, y_ratio):
         """
         Given floating point coordinates between (0.0, 0.0) and (1.0, 1.0),
@@ -29,25 +129,6 @@ class SlammerCtrl(object):
 
         return pix_x, pix_y
 
-    def plot(self, x, y, color):
-        """
-        Plot a pixel on the canvas in the specified color. If pixel specified
-        is out of bounds, do nothing.
-        """
-        canvas_w, canvas_h = self.model.canvas.get_size()
-        tile_w, tile_h = self.model.canvas.get_tile_size()
-
-        max_x = canvas_w * tile_w
-        max_y = canvas_h * tile_h
-        if x < 0 or y < 0 or x >= max_x or y >= max_y:
-            return
-
-        tile_x, tile_y = x // tile_w, y // tile_h
-        tile_pix_x, tile_pix_y = x % tile_w, y % tile_h
-
-        tile = self.model.canvas.get_tile(tile_x, tile_y)
-        tile.set_pixel(tile_pix_x, tile_pix_y, color)
-
     def on_canvas_click(self, x, y, buttons, modifiers):
         """
         When a spot on the canvas is clicked, this method is notified with x
@@ -56,12 +137,13 @@ class SlammerCtrl(object):
         #identify the right pixel
         pix_x, pix_y = self.get_canvas_pixel(x, y)
 
-        if buttons & pyglet.window.mouse.LEFT:
-            #turn it black
-            self.plot(pix_x, pix_y, (0,0,0,255))
-        elif buttons & pyglet.window.mouse.RIGHT:
-            #turn it white
-            self.plot(pix_x, pix_y, (255,255,255,255))
+        if not self.current_tool_instance:
+            self.current_tool_instance = self.current_tool(self.current_color)
+        more_input = self.current_tool_instance.accept_click(pix_x, pix_y)
+        if not more_input:
+            self.current_tool_instance.do(self.model)
+            self.current_tool_instance = None
+
 
     def on_canvas_drag(self, start_x_ratio, start_y_ratio, end_x_ratio,
                        end_y_ratio, buttons, modifiers):
@@ -69,40 +151,15 @@ class SlammerCtrl(object):
         When the user drags the mouse starting in the canvas, this method is
         notified with x and y floating point coordinates for both start and
         end positions.
-
-        Also happens to be an implementation of the Bresenham line algorithm.
         """
-        if pyglet.window.mouse.LEFT & buttons:
-            color = (0, 0, 0, 255)
-        elif pyglet.window.mouse.RIGHT & buttons:
-            color = (255, 255, 255, 255)
-        else:
-            return
 
         start_x, start_y = self.get_canvas_pixel(start_x_ratio, start_y_ratio)
         end_x, end_y = self.get_canvas_pixel(end_x_ratio, end_y_ratio)
 
-        steep = abs(end_y - start_y) > abs(end_x - start_x)
-        if steep:
-            start_x, start_y = start_y, start_x
-            end_x, end_y = end_y, end_x
-        if start_x > end_x:
-            start_x, end_x = end_x, start_x
-            start_y, end_y = end_y, start_y
-        delta_x = end_x - start_x
-        delta_y = abs(end_y - start_y)
-        error = delta_x / 2
-        y = start_y
-        if start_y < end_y:
-            y_step = 1
-        else:
-            y_step = -1
-        for x in xrange(start_x, end_x+1):
-            if steep:
-                self.plot(y, x, color)
-            else:
-                self.plot(x, y, color)
-            error -= delta_y
-            if error < 0:
-                y += y_step
-                error += delta_x
+        if not self.current_tool_instance:
+            self.current_tool_instance = self.current_tool(self.current_color)
+        more_input = self.current_tool_instance.accept_drag(start_x, start_y,
+                                                            end_x, end_y)
+        if not more_input:
+            self.current_tool_instance.do(self.model)
+            self.current_tool_instance = None
