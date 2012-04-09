8aede22cbf101e3239670062031598e1765a0cef
Cameron Seebach <cameron.seebach@gmail.com>
2012-04-08 18:50:13 -0700

    removed autoflush on Tile unpickling, added docstrings to it's methods and a TODO for implementing autoflushing on access that requires it

--- model.py
+++ model.py
@@ -8,25 +8,45 @@ import pyglet
 def tile_unpickler(width, height, data):
     t = Tile(width, height)
     t.ctypes_data[:] = data
-    t.flush_changes()
     return t
+
 
 class Tile(pyglet.image.ImageData):
     """
     Represents a drawing surface with pixel access.
     """
 
+    # TODO autoflush changes on any access that requires it
+
     def __init__(self, width, height):
         #noinspection PyCallingNonCallable,PyTypeChecker
         self.ctypes_data = (ctypes.c_ubyte * (width * height * 4))()
         super(Tile, self).__init__(width, height, "RGBA", ctypes.pointer(self.ctypes_data))
 
+    def get_pixel(self, x, y):
+        """
+        Get a pixel, as a sequence of 4 color components in RGBA order.
+
+        Each color component is between 0 and 255.
+        """
+        pitch = self.width * 4
+        offset = (y * pitch) + x * 4
+        return self.ctypes_data[offset:offset+4]
+
     def set_pixel(self, x, y, color):
+        """
+        Set a pixel from a sequence of 4 color components in RGBA order.
+
+        Each color component must be between 0 and 255.
+        """
         pitch = self.width * 4
         offset = (y * pitch) + x * 4
         self.ctypes_data[offset:offset+4] = color
 
     def flush_changes(self):
+        """
+        Changes made with set_pixel are not actually seen until this method is called.
+        """
         self.set_data("RGBA", self.width * 4, ctypes.pointer(self.ctypes_data))
 
     #noinspection PyMethodOverriding
